7d83822941a240722d9f91714609a03012367f1e
Jeffter Mayron Ferreira Olírio <jefftermayron@gmail.com>
2023-02-09 00:03:22 -0300

    Algoritmos de busca

---
+++ aulaalgoritmosdebusca/aulabuscasequencial.py
@@ -0,0 +1,15 @@
+def busca_sequencial(lista, elemento):
+    pos = 0 
+    encontrado = False
+     
+    while pos < len(lista) and not encontrado:
+        if lista[pos] == elemento:
+            encontrado = True
+        else:
+            pos = pos+1
+  
+    return encontrado
+
+testelista = [2, 10, 8, 15, 18, 20, 12, 1]
+print(busca_sequencial(testelista, 5))
+print(busca_sequencial(testelista, 15))
